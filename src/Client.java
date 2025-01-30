4961cfa287b1a836c5dc74fc8e709ab3e0b495e6
Gab2006 <tostigabriele6@gmail.com>
2025-01-30 13:32:31 +0100

    Secondo commit

--- src/Client.java
+++ src/Client.java
@@ -1,13 +1,28 @@
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Client{
     private String nome;
     private String colore; 
-
+    private Socket socket;
+    
     public Client(String nomeDefault, String coloreDefault){
         nome = nomeDefault;
         colore = coloreDefault;
     }
+    public Client(String nome){
+        this.nome = nome;
+    }
     public void connetti(String nomeServer, int portaServer){
-
+        try {
+            socket = new Socket(nomeServer, portaServer);
+            System.out.println("1. Connessione avvenuta.");
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.err("Connessione non riuscita.");
+        }
     }
     public void scrivi(){
 
--- src/MainCLient.java
+++ src/MainCLient.java
@@ -1,5 +1,8 @@
 public class MainClient{
     public static void main(String[] args) {
-        
+        Client cl = new Client("Tosti");
+        cl.connetti("localhost", 2000);
+        cl.scrivi();
+        cl.chiudi();
     }
 }
